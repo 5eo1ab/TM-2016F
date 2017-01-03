498c4975d69fd75b7abce5a75038d99e7ef4d1fb
Hanbin Seo <myhb9188@seoultech.ac.kr>
2017-01-03 21:08:25 +0900

    Update proc_topic_analysis.py

--- P_code/proc_topic_analysis.py
+++ P_code/proc_topic_analysis.py
@@ -8,14 +8,10 @@ Created on Thu Dec 15 05:08:45 2016
 import pandas as pd
 from pandas import DataFrame as df
 
-save_dir = "D:/course_2016_fall/Unstructured_Data_Analysis/Project/data/"
+save_dir = "../P_data/"
 dataset = pd.read_csv(save_dir+"dataset_v4.csv")
 doc_topic = pd.read_csv(save_dir+"lda_prob_doc-topic.csv")
 topic_term = pd.read_csv(save_dir+"lda_prob_topic-term.csv")
-
-
-
-
 
 doc_topic['year'] = dataset['year']
 year_topic = doc_topic.groupby(['year']).mean()
@@ -47,29 +43,5 @@ from sklearn.metrics import pairwise_distances
 from scipy.spatial.distance import cosine
 dist_out = 1-pairwise_distances(pub_topic, metric="cosine")
 df(dist_out, columns=pub_topic.index).to_csv(save_dir+"pub_cosine_matrix.csv", index=False)
-
-"""
-from sklearn.decomposition import TruncatedSVD
-from sklearn.cluster import KMeans, MiniBatchKMeans
-
-svd=TruncatedSVD(n_components=2)
-svd_result=svd.fit_transform(pub_topic) 
-print svd_result
- 
-km=KMeans(n_clusters=3)
-km.fit(svd_result)
-
-print "labels>>>>", km.labels_
-print "cluster_centers>>>>", km.cluster_centers_
-
-from pylab import plot,show, legend
-color = ['or', 'og', 'ob', 'oc', 'om', 'oy', 'ok', 'ow']
-for num in range(0, 3):
-	plot(svd_result[km.labels_==num,0],svd_result[km.labels_==num,1],color[num%7], label='group'+str(num))
-legend(loc='upper right')
-show()
-"""
-
-
 
  
